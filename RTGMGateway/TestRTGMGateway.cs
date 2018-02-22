21eae75cd573cdf3f4ee3930254b39a6a08a0fd9
Iván Trujillo <itrujillo@softgrade.mx>
2018-02-22 13:53:13 -0600

    Pruebas unitarias creadas

--- RTGMGateway/TestRTGMGateway.cs
+++ RTGMGateway/TestRTGMGateway.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace RTGMGateway
 {
+    [TestFixture]
     class TestRTGMGateway
     {
+        [TestCase(502602197, "GMO PM ROSTICERIAS MI PAN SA DE CV")]
+        [TestCase(502602198, "BLAS RAMIREZ LUNA")]
+        [TestCase(null, "Venta al publico")]
+        [TestCase(0, "Venta al publico")]
+        public void pruebaRecuperaCliente(int Cliente, string Nombre)
+        {
+            RTGMGateway objGateway = new RTGMGateway();
+            objGateway.URLServicio = @"http://192.168.1.30:88/GasMetropolitanoRuntimeService.svc";
+            SolicitudGateway objRequest = new SolicitudGateway { Fuente = RTGMCore.Fuente.Sigamet, IDCliente = Cliente, IDEmpresa = 0, Portatil = false, IDAutotanque = 52 };
+
+            RTGMCore.DireccionEntrega objDireccionEntega = objGateway.buscarDireccionEntrega(objRequest);
+
+            Assert.AreEqual(objDireccionEntega.Nombre.Trim(), Nombre);
+        }
     }
 }
