5ae3715fe887d3d08a6d6b93895e73bded8674ca
Ruben Morera <rubencho_01@hotmail.com>
2018-08-03 16:53:19 -0500

    1. Se agrega el método Exportar en la clase Utilerias 2. Se agrega el enumerador EnumMetodoWS

--- RTGMGateway/Utilerias.cs
+++ RTGMGateway/Utilerias.cs
@@ -8,18 +8,58 @@ using System.Xml.Serialization;
 
 namespace RTGMGateway
 {
+    public enum EnumMetodoWS
+    {
+        BusquedaDireccionEntrega = 1,
+        ConsultarPedidos = 2,
+        ActualizarPedido = 3
+    }
+    
     public class Utilerias
     {
-        public static void ExportarAXML(object objeto, string ruta, bool anexar = false)
+        public static void Exportar(object obSolicitud, object obRespuesta, RTGMCore.Fuente fuente, bool exitoso, EnumMetodoWS metodo)
         {
-            StringWriter stringWriter = new StringWriter();
+            string rutaConsultaDireccion = AppDomain.CurrentDomain.BaseDirectory
+                    + "\\Log\\" + "BusquedaDireccionEntrega" + fuente.ToString().ToUpper() + (exitoso ? "_EXITOSO.xml" : "_FALLIDO.xml");
+
+            string rutaConsultaPedido = AppDomain.CurrentDomain.BaseDirectory
+                    + "\\Log\\" + "ConsultarPedidos" + fuente.ToString().ToUpper() + (exitoso ? "_EXITOSO.xml" : "_FALLIDO.xml");
+
+            string rutaActualizarPedido = AppDomain.CurrentDomain.BaseDirectory
+                    + "\\Log\\" + "ActualizarPedido" + fuente.ToString().ToUpper() + (exitoso ? "_EXITOSO.xml" : "_FALLIDO.xml");
+
+            switch (metodo)
+            {
+                case EnumMetodoWS.BusquedaDireccionEntrega:
+                    Utilerias.ExportarAXML(obSolicitud, rutaConsultaDireccion);
+                    Utilerias.ExportarAXML(obRespuesta, rutaConsultaDireccion, true);
+                    break;
+
+                case EnumMetodoWS.ConsultarPedidos:
+                    Utilerias.ExportarAXML(obSolicitud, rutaConsultaPedido);
+                    Utilerias.ExportarAXML(obRespuesta, rutaConsultaPedido, true);
+                    break;
+
+                case EnumMetodoWS.ActualizarPedido:
+                    Utilerias.ExportarAXML(obSolicitud, rutaActualizarPedido);
+                    Utilerias.ExportarAXML(obRespuesta, rutaActualizarPedido, true);
+                    break;
+            }
+
+        }
+
+        private static void ExportarAXML(object objeto, string ruta, bool anexar = false)
+        {
             var writer = new System.IO.StreamWriter(ruta, anexar);
+            XmlTextWriter textWriter = new XmlTextWriter(writer);
+            textWriter.Formatting = Formatting.Indented;
+            textWriter.IndentChar = '\t';
+            textWriter.Indentation = 1;
 
             try
             {
                 XmlSerializer serializer = new XmlSerializer(objeto.GetType());
-                //XmlSerializer serializer = new XmlSerializer(typeof(RTGMCore.DireccionEntregaSIGAMETDatos));
-                serializer.Serialize(writer, objeto);
+                serializer.Serialize(textWriter, objeto);
 
                 writer.Flush();
             }
@@ -29,7 +69,7 @@ namespace RTGMGateway
             }
             finally
             {
-                stringWriter.Close();
+                textWriter.Close();
                 if (writer != null)
                 {
                     writer.Close();
@@ -37,5 +77,6 @@ namespace RTGMGateway
                 }
             }
         }
+
     }
 }
