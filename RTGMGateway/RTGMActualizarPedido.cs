5db919546b4d844ad18b5472a7525c15b299c113
Ruben Morera <rubencho_01@hotmail.com>
2018-08-10 12:41:34 -0500

    Se agrega la funcionalidad de consultar Fuente, Corporativo y Sucursal en la clase RTGMActualizarPedido

--- RTGMGateway/RTGMActualizarPedido.cs
+++ RTGMGateway/RTGMActualizarPedido.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -8,28 +9,140 @@ namespace RTGMGateway
 {
     public class RTGMActualizarPedido
     {
-        public string URLServicio { get; set; }
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private RTGMCore.GasMetropolitanoRuntimeServiceClient serviceClient;
+        private BasicHttpBinding _BasicHttpBinding;
+        private EndpointAddress _EndpointAddress;
         private double longitudRepuesta;
         private int tiempoEspera;
         private bool guardarLog;
+        private const int MAX_CAPACITY = 2147483647;
+        private RTGMCore.Fuente _Fuente;
+        private string _CadenaConexion;
+        private byte _Corporativo;
+        private byte _Sucursal;
+        private byte _Modulo;
+
+        #region PROPIEDADES
+
+        public string URLServicio { get; set; }
+
+        public RTGMCore.Fuente Fuente
+        {
+            get
+            {
+                return _Fuente;
+            }
+        }
+
+        public byte Corporativo
+        {
+            get
+            {
+                return _Corporativo;
+            }
+        }
+
+        #endregion
+
+        public RTGMActualizarPedido(byte Modulo, string CadenaConexion)
+        {
+            // Inicializar logger
+            log4net.Config.XmlConfigurator.Configure();
+
+            _BasicHttpBinding = new BasicHttpBinding();
+            _BasicHttpBinding.MaxReceivedMessageSize = MAX_CAPACITY;
+            _BasicHttpBinding.MaxBufferSize = MAX_CAPACITY;
+
+            _Modulo = Modulo;
+            _CadenaConexion = CadenaConexion;
+            consultarParametros(_Modulo, _CadenaConexion);
+
+            log.Info("Una nueva instancia de ActualizarPedido ha sido creada.");
+        }
+
+        #region METODOS DE CLASE
+        
+        /// <summary>
+        /// Registra los parámetros en el archivo log
+        /// </summary>
+        /// <param name="ParNombreMetodo">Nombre del método que se va a ejecutar</param>
+        /// <param name="ParSolicitud">Objeto del tipo SolicitudPedidoGateway</param>
+        private void registrarParametros(SolicitudActualizarPedido ParSolicitud)
+        {
+            log.Info("Inicia llamado a ActualizarPedido" +
+                    ", ID Empresa: "                + _Corporativo + 
+                    ", Source: "                    + _Fuente +
+                    ", Pedidos: "                   + ParSolicitud.Pedidos.Count + 
+                    ", Portatil: "                  + ParSolicitud.Portatil +
+                    ", Tipo de actualización: "     + ParSolicitud.TipoActualizacion + 
+                    ", Usuario: "                   + ParSolicitud.Usuario);
+        }
+
+        /// <summary>
+        /// Consulta los parámetros FuenteCRM, Corporativo y Sucursal mediante una instancia de la 
+        /// clase DAO
+        /// </summary>
+        /// <param name="modulo"></param>
+        /// <param name="cadenaConexion"></param>
+        private void consultarParametros(byte modulo, string cadenaConexion)
+        {
+            DataRow drParametros;
+            DAO obDAO = new DAO(modulo, cadenaConexion);
+
+            _Fuente = obDAO.consultarFuente(modulo, cadenaConexion);
+            drParametros = obDAO.consultarCorporativoSucursal(modulo, cadenaConexion);
+
+            if (drParametros != null)
+            {
+                _Corporativo = Convert.ToByte(drParametros["Corporativo"]);
+                _Sucursal = Convert.ToByte(drParametros["Sucursal"]);
+            }
+            else
+            {
+                _Corporativo = 0;
+                _Sucursal = 0;
+            }
+        }
+
+        /// <summary>
+        /// Ejecuta el método serviceClient.ActualizarPedido()
+        /// </summary>
+        /// <param name="ParSolicitud">Objeto del tipo SolicitudPedidoGateway</param>
+        private List<RTGMCore.Pedido> actualizarPedidos(SolicitudActualizarPedido ParSolicitud)
+        {
+            return serviceClient.ActualizarPedido(  _Fuente,                            _Corporativo, 
+                                                    ParSolicitud.TipoActualizacion,     ParSolicitud.Portatil, 
+                                                    ParSolicitud.Pedidos,               ParSolicitud.Usuario);
+        }
+
+        #endregion
 
         public List<RTGMCore.Pedido> ActualizarPedido(SolicitudActualizarPedido Solicitud)
         {
-            List<RTGMCore.Pedido> lstPedidosRepuesta = new List<RTGMCore.Pedido>();
-            
+            List<RTGMCore.Pedido> lstPedidosRespuesta = new List<RTGMCore.Pedido>();
 
-            log.Info("Inicia ejecución de método ActualizarPedido");
-            System.ServiceModel.BasicHttpBinding basicHttpBinding = new BasicHttpBinding();
-            System.ServiceModel.EndpointAddress endpointAddress = new EndpointAddress(this.URLServicio);
-            serviceClient = new RTGMCore.GasMetropolitanoRuntimeServiceClient(basicHttpBinding, endpointAddress);
+            try
+            {
+                log.Info("Inicia ejecución de método ActualizarPedido");
 
-            lstPedidosRepuesta = serviceClient.ActualizarPedido(Solicitud.Fuente, Solicitud.IDEmpresa, Solicitud.TipoActualizacion, Solicitud.Portatil, Solicitud.Pedidos, Solicitud.Usuario);
+                _EndpointAddress = new EndpointAddress(this.URLServicio);
 
-            lstPedidosRepuesta.ForEach(x => x.Message = "NO HAY ERROR");
+                serviceClient = new RTGMCore.GasMetropolitanoRuntimeServiceClient(_BasicHttpBinding, _EndpointAddress);
 
-            return lstPedidosRepuesta;
+                registrarParametros(Solicitud);
+
+                lstPedidosRespuesta = actualizarPedidos(Solicitud);
+
+                lstPedidosRespuesta.ForEach(x => x.Message = "NO HAY ERROR");
+
+                lstPedidosRespuesta.ForEach(x => Utilerias.SerializarAString(x));
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message, ex);
+            }
+            return lstPedidosRespuesta;
         }
     }
 }
