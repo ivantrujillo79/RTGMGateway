0afd3c873fd8ad94f79427c0d4479d24fdb159e9
Ruben Morera <rubencho_01@hotmail.com>
2018-08-10 12:45:34 -0500

    Se agrega funcionalidad para exportar archivo XML en clase de prueba

--- RTGMGateway/Test/TestRTGMActualizaPedido.cs
+++ RTGMGateway/Test/TestRTGMActualizaPedido.cs
@@ -9,11 +9,16 @@ namespace RTGMGateway
     [TestFixture]
     class TestRTGMActualizaPedido
     {
+        private string _CadenaConexion = "Server=192.168.1.30;Database=sigametdevtb;User Id=ROPIMA;Password = ROPIMA9999;";
+        private byte _Modulo = 1;
+
         [TestCase(RTGMCore.Fuente.Sigamet)]
         [TestCase(RTGMCore.Fuente.CRM)]
         public void pruebaActualizaPedido(RTGMCore.Fuente FuentePrueba)
         {
-            RTGMActualizarPedido objGateway = new RTGMActualizarPedido();
+            bool respuestaExitosa = true;
+
+            RTGMActualizarPedido objGateway = new RTGMActualizarPedido(_Modulo, _CadenaConexion);
             objGateway.URLServicio = @"http://192.168.1.30:88/GasMetropolitanoRuntimeService.svc";
 
             List<RTGMCore.Pedido> lstPedido = new List<RTGMCore.Pedido>();
@@ -23,21 +28,42 @@ namespace RTGMGateway
             lstPedido.Add(new RTGMCore.PedidoCRMSaldo { IDPedido = 6165, IDZona = 6, AnioPed = 2018, Abono = 200, PedidoReferencia = "6165" });
             lstPedido.Add(new RTGMCore.PedidoCRMSaldo { IDPedido = 6166, IDZona = 6, AnioPed = 2018, Abono = 200, PedidoReferencia = "6166" });
             
-            SolicitudActualizarPedido Solicitud = new SolicitudActualizarPedido { Fuente = FuentePrueba, IDEmpresa = 1, Pedidos = lstPedido, Portatil = false, TipoActualizacion = RTGMCore.TipoActualizacion.Saldo, Usuario = "ROPIMA" };
+            SolicitudActualizarPedido Solicitud = new SolicitudActualizarPedido
+            {
+                //Fuente = FuentePrueba,
+                //IDEmpresa = 1,
+                Pedidos = lstPedido,
+                Portatil = false,
+                TipoActualizacion = RTGMCore.TipoActualizacion.Saldo,
+                Usuario = "ROPIMA"
+            };
 
             List<RTGMCore.Pedido> ListaRespuesta = objGateway.ActualizarPedido(Solicitud);
 
-            Assert.IsNotNull(ListaRespuesta);
-            Assert.AreEqual(ListaRespuesta.Count, 5);       
+            try
+            {
+                Assert.IsNotNull(ListaRespuesta[0]);
+            }
+            catch (Exception)
+            {
+                respuestaExitosa = false;
+            }
+
+            Utilerias.Exportar(Solicitud, ListaRespuesta, objGateway.Fuente, respuestaExitosa, EnumMetodoWS.ActualizarPedido);
+
         }
     }
 
     class TestRTGMActualizaPedidoBoletin
     {
+        private string _CadenaConexion = "Server=192.168.1.30;Database=sigametdevtb;User Id=ROPIMA;Password = ROPIMA9999;";
+        private byte _Modulo = 1;
+
         [TestCase]
         public void pruebaActualizaPedidoBoletin()
         {
-            RTGMActualizarPedido objGateway = new RTGMActualizarPedido();
+            bool respuestaExitosa = true;
+            RTGMActualizarPedido objGateway = new RTGMActualizarPedido(_Modulo, _CadenaConexion);
             objGateway.URLServicio = @"http://192.168.1.30:88/GasMetropolitanoRuntimeService.svc";
 
             List<RTGMCore.Pedido> lstPedido = new List<RTGMCore.Pedido>();
@@ -47,13 +73,28 @@ namespace RTGMGateway
             lstPedido.Add(new RTGMCore.PedidoCRMSaldo { IDPedido = 6165, IDZona = 6, AnioPed = 2018, PedidoReferencia = "6165" });
             lstPedido.Add(new RTGMCore.PedidoCRMSaldo { IDPedido = 6166, IDZona = 6, AnioPed = 2018, PedidoReferencia = "6166" });
 
-            SolicitudActualizarPedido Solicitud = new SolicitudActualizarPedido { Fuente = RTGMCore.Fuente.Sigamet, IDEmpresa = 1, Pedidos = lstPedido, Portatil = false, TipoActualizacion = RTGMCore.TipoActualizacion.Boletin, Usuario = "ROPIMA" };
+            SolicitudActualizarPedido Solicitud = new SolicitudActualizarPedido
+            {
+                //Fuente = RTGMCore.Fuente.Sigamet,
+                //IDEmpresa = 1,
+                Pedidos = lstPedido,
+                Portatil = false,
+                TipoActualizacion = RTGMCore.TipoActualizacion.Boletin,
+                Usuario = "ROPIMA"
+            };
 
             List<RTGMCore.Pedido> ListaRespuesta = objGateway.ActualizarPedido(Solicitud);
 
-            Assert.IsNotNull(ListaRespuesta);
-            Assert.AreEqual(ListaRespuesta.Count, 5);
+            try
+            {
+                Assert.IsNotNull(ListaRespuesta[0]);
+            }
+            catch (Exception)
+            {
+                respuestaExitosa = false;
+            }
 
+            Utilerias.Exportar(Solicitud, ListaRespuesta, objGateway.Fuente, respuestaExitosa, EnumMetodoWS.ActualizarPedido);
 
         }
 
