44a0f203382a2624f71db86f687f037eb50d42d3
Ruben Morera <rubencho_01@hotmail.com>
2018-09-18 13:13:02 -0500

    Se agrega la clase estática Variables.cs

---
+++ Pruebas/Variables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    public static class Variables
+    {
+        public static string GLOBAL_CadenaConexion = "Server=192.168.1.30;Database=SigametDEVTB;User Id=ROPIMA;Password = ROPIMA9999;";
+        public static string GLOBAL_URLGateway = @"http://192.168.1.21:88/GasMetropolitanoRuntimeService.svc";
+        public static byte GLOBAL_Modulo = 1;
+    }
+}
