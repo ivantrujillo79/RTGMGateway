2f2eb42a79016c301b7f2140bd54b6b8ee50ddb8
Ruben Morera <rubencho_01@hotmail.com>
2018-10-10 13:42:50 -0500

    Se actualiza el servidor al que se conecta el proyecto de pruebas

--- Pruebas/Variables.cs
+++ Pruebas/Variables.cs
@@ -8,7 +8,7 @@ namespace Pruebas
 {
     public static class Variables
     {
-        public static string GLOBAL_CadenaConexion = "Server=192.168.1.30;Database=SigametDEVTB;User Id=ROPIMA;Password = ROPIMA9999;";
+        public static string GLOBAL_CadenaConexion = "Server=192.168.1.26;Database=SigametCRM;User Id=ROPIMA;Password = ROPIMA9999;";
         public static string GLOBAL_URLGateway = @"http://192.168.1.21:88/GasMetropolitanoRuntimeService.svc";
         public static byte GLOBAL_Modulo = 1;
     }
